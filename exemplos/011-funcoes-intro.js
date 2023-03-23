c688a16d938f21dd6090b4b07a9470d3ff54b07f
gabriel.cervantess <gabriel.almeida279@etec.sp.gov.br>
2023-03-23 20:29:36 -0300

    aula_23.03

--- exemplos/011-funcoes-intro.js
+++ exemplos/011-funcoes-intro.js
@@ -7,8 +7,8 @@ function data () {
 
 // função sem parâmetro e com retorno
 function caracterAleatorio () {
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; // 61 itens
-    const nAleatorio = Math.floor(Math.random() * (61 - 0 + 1) + 0); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; // 62 itens
+    const nAleatorio = Math.floor(Math.random() * (62 - 0 + 1) + 0); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     const caracter = characters.charAt(nAleatorio); 
     return caracter;
     // https://www.programiz.com/javascript/examples/generate-random-strings
@@ -23,3 +23,22 @@ function olaNome (nome = 'não foi inserido um nome') {
 function soma (n1 = 0, n2 = 0) {
     return n1 + n2;
 }
+
+// função com parâmetro com retorno
+function soma (n1 = 0, n2 = 0) {
+    return n1 * n2;
+}
+
+// função sem parâmetro e sem retorno
+data();
+
+//função sem parâmetro e com retorno
+const cAleatório = caracterAleatorio();
+console.log(cAleatorio);
+
+// função com parâmetro sem retorno
+olaNome();
+
+// função com parâmetro com retorno
+const resultado = soma(1, 5);
+console.log(resultado);
